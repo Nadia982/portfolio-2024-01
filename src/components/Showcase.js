d9a7c40c20f16124601cbdf6a5aa753c5b84e151
Natalie Gillam <ngillam@hotmail.co.uk>
2024-01-10 10:14:01 +0000

    added content to left-hand side of Showcase.js (hero section)

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -7,14 +7,35 @@ import {
   IconContainer,
 } from "../styles/Global.styled";
 
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+
 const Showcase = () => {
   return (
     <PaddingContainer id="Home" left="3%" right="10%" top="15%" bottom="10%">
       <FlexContainer align="center" fullWidthChild>
         {/* --left-content-- */}
         <div>
-          <Heading as="h4" size="h4">Hello!</Heading>
+          <Heading as="h1" size="h1" top="0.5rem" bottom="1rem">
+            Natalie Gillam -<BlueText> React Developer</BlueText>
+          </Heading>
+          <ParaText as="p" top="2rem" bottom="4rem">
+            I am a frontend web developer based near London, UK and have been
+            building websites with React and JavaScript since 2022. I love
+            creating sites and apps that solve real-world problems, while
+            engaging and delighting users.
+          </ParaText>
+
+          {/* --Social icons-- */}
+          <FlexContainer gap="20px">
+            <IconContainer color="blue" size="1.5rem">
+              <BsLinkedin />
+            </IconContainer>
+            <IconContainer color="blue" size="1.5rem">
+              <BsGithub />
+            </IconContainer>
+          </FlexContainer>
         </div>
+        
       </FlexContainer>
     </PaddingContainer>
   );
--- src/styles/Global.styled.js
+++ src/styles/Global.styled.js
@@ -33,7 +33,7 @@ export const Heading = styled(PaddingContainer)`
   font-size: ${({ size }) => {
     switch (size) {
       case "h1":
-        return "4.5rem";
+        return "3rem";
 
       case "h2":
         return "3rem";
